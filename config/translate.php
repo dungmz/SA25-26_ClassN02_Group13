bfc496303b2fa8a713f62c772de7fbc3a500ec3a
dungmz <23010568@st.phenikaa-uni.edu.vn>
2026-02-07 01:15:11 +0700

    Update Google Translate API key in config

--- config/translate.php
+++ config/translate.php
@@ -2,5 +2,5 @@
 // Google Cloud Translation API settings.
 // Set GOOGLE_TRANSLATE_API_KEY to your API key.
 
-define('GOOGLE_TRANSLATE_API_KEY', 'AIzaSyD6GUstfKdKILqfHrN8c76sjrpwlq6Oawc');
+define('GOOGLE_TRANSLATE_API_KEY', 'api_key_cua_ban');
 define('GOOGLE_TRANSLATE_ENDPOINT', 'https://translation.googleapis.com/language/translate/v2');
